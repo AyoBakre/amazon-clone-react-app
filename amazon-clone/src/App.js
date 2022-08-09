9a79f2d440cfa42642af9cc285b6b52afd4ce05a
AyoBakre <ayomidebakre@gmail.com>
2022-08-09 02:10:06 +0100

    remove unneccessary files

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="app">
+      <h1>ickambu</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBH37i_hKrlhhGlspwz4UuAbMLbmbg0v2U",
+    authDomain: "clone-8d7ce.firebaseapp.com",
+    projectId: "clone-8d7ce",
+    storageBucket: "clone-8d7ce.appspot.com",
+    messagingSenderId: "105356010865",
+    appId: "1:105356010865:web:3f66684854c8fe034702d0",
+    measurementId: "G-3WX2V7L5SP"
+  };
